b83f545fbda94e8373a55701c214081ad54b6a84
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-09-15 20:35:11 +0200

    Add: Styles to component Header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,17 @@
 import './Footer.scss';
 
 function Footer () {
-  return <div className="footer">Footer</div>
+  return (
+    <div className="footer">
+      <h3>Eco-store</h3>
+      <p>Careers</p>
+      <p>Careers</p>
+      <p>Careers</p>
+      <div className='social-media'>
+        <img src='' alt=''/>
+      </div>
+    </div>
+  )
 }
 
 export { Footer }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,60 @@
 import './Main.scss'
 
 function Main () {
-  return <main >Main</main>
+  return (
+    <div className='main-section'>
+      <article>
+        <div className='impact-section'>
+          <h1>Impacto Ecológico</h1>
+          <p>Debido al impacto ambiental siendo las acciones humanas las que  modifican el ambiente, Eco-store se compromete a elaborar sus productos de manera artesanal y con materiales que favorecen al cuidado del medio ambiente.</p>
+        </div>
+        <button>Conoce mas</button>
+        <div className='image-section '>
+        <img src="" alt=""/>
+        </div>
+      </article>
+      <section className="product-section">
+            <div className="healthcare">
+                <h2>Cuidado de la salud</h2>
+            </div>
+            <div className="furniture">
+                <h2>Decoracion del hogar</h2>
+            </div>
+        </section>
+        <section className="info-section">
+            <div className="about-us">
+                <h2>¿Por qué nosotros?</h2>
+                <div className="earty">
+                    <img src="" alt=""/>
+                    <p></p>
+                </div>
+                <div className="innovation">
+                    <img src="" alt=""/>
+                    <p></p>
+                </div>
+            </div>
+            <div className="gallery">
+                <h2>Galería de estilos</h2>
+            </div>
+            <div>
+              <img src='' alt=''/>
+            </div>
+            <div>
+              <img src='' alt=''/>
+            </div>
+            <div>
+              <img src='' alt=''/>
+            </div>
+            <div className="location">
+                <h2>Encuéntranos</h2>
+                <div className="map">
+                    <img src="" alt=""/>
+                </div>
+                <p></p>
+            </div>
+        </section>
+    </div>
+  )
 }
 
 export { Main }
